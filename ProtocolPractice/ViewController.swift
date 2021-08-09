46e295b5f2142125e4b981531ff436c4fe78bae2
kapisan <60970645+kapisan@users.noreply.github.com>
2021-08-09 16:32:16 +0900

    Initial Commit

---
+++ ProtocolPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProtocolPractice
+//
+//  Created by niwa  shuhei on 2021/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
