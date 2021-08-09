0dffc170eeea243a58d86458ffda4552a6890373
kapisan <60970645+kapisan@users.noreply.github.com>
2021-08-09 18:31:07 +0900

    Storyboardとコードの紐付け完了

--- ProtocolPractice/ViewController.swift
+++ ProtocolPractice/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+
     }
+
+    @IBAction func buttonAction1(_ sender: UIButton) {
+    }
+
+    @IBAction func buttonAction2(_ sender: UIButton) {
+    }
+
 
 
 }
 
+protocol SampleProtocol {
+
+}
