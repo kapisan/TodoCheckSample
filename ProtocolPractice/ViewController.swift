7f35bc6bf4b4a9d56168e7d8e548ae0d4e865448
kapisan <60970645+kapisan@users.noreply.github.com>
2021-08-09 20:59:54 +0900

    buttonを押すとチェックマークのオンオフを切り替えれるようにした

--- ProtocolPractice/ViewController.swift
+++ ProtocolPractice/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     let okImage = UIImage(named: "ok")
     let ngImage = UIImage(named: "ng")
 
+    var switch_flag1: Bool = false
+    var switch_flag2: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,9 +29,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonAction1(_ sender: UIButton) {
+
+        if(switch_flag1 == false) {
+            switch_flag1 = true
+            button1.setImage(okImage, for: .normal)
+        } else {
+            switch_flag1 = false
+            button1.setImage(ngImage, for: .normal)
+        }
     }
 
     @IBAction func buttonAction2(_ sender: UIButton) {
+
+        if(switch_flag2 == false) {
+            switch_flag2 = true
+            button2.setImage(okImage, for: .normal)
+        } else {
+            switch_flag2 = false
+            button2.setImage(ngImage, for: .normal)
+        }
     }
 
 
